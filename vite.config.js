6f44fa0bb2f00c2b3ddde3f5329d671abcabb1b4
Mridul Sharma <sharmamridul045@gmail.com>
2023-08-10 15:47:34 +0530

    file updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://github.com/mriduldevelops/Admin-Dashboard.git",
+  base: "/Admin-Dashboard/",
   plugins: [react()],
 })
